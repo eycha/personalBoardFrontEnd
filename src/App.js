9027ea8ba5cc7a752178634da85fc262eae53bc4
eycha <silvlexonst@gmail.com>
2023-04-11 17:52:14 +0900

    fix: root render fix

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Blog</h1>
+      <div className='blog-container'>
+        <h2>제목</h2>
+        <div>
+          내용
+        </div>
+      </div>
+      <div className='form-wrapper'>
+        <input className="title-input" type="text" placeholder='제목'/>
+        <textarea className='text-area' placeholder='내용'></textarea>
+      </div>
+      <button className='submit-button'>입력</button>        
+    </div>
+  );
+}
+
+export default App;
