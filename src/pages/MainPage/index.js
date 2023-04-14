1dcd56285b451f308c71334527bcc52f0562bb75
eycha <silvlexonst@gmail.com>
2023-04-14 17:59:41 +0900

    chore: post method edit

--- src/pages/MainPage/index.js
+++ src/pages/MainPage/index.js
@@ -5,12 +5,21 @@ import axios from "../../api/axios";
 const MainPage = () => {
 
   
-  // const [posts,setPosts] = useState({
-  //   id:"",
-  //   title:"",
-  //   content:"",
+  const [post,setPost] = useState({
+    id:"",
+    title:"",
+    content:"",
 
-  // });
+  });
+
+  const [posts, setPosts] = useState([
+    {id:1, title:'제목1',content:'내용1'},
+    {id:2, title:'제목2',content:'내용2'},
+    {id:3, title:'제목3',content:'내용3'},
+
+
+
+  ])
   const [modalOpen, setModalOpen] = useState(false)
   
   const handleClick = () => {
@@ -20,7 +29,7 @@ const MainPage = () => {
   return (
   <div className="App">
     <div className="list">
-      <h3>제목</h3>
+      <h3>{post.title}</h3>
       <p>리스트 내용</p>
       <hr/>
     </div>
