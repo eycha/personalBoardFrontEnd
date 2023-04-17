b1e2e567eb9b17d27d75d995c92d526ff7e51b1a
eycha <silvlexonst@gmail.com>
2023-04-17 12:04:11 +0900

    feat: Posting used API

--- src/components/WriteModal/index.js
+++ src/components/WriteModal/index.js
@@ -1,37 +1,57 @@
 import React, {useState} from "react";
+import { json } from "react-router-dom";
 import styled from "styled-components";
 
 
 const WriteModal = ({setModalOpen}) => {
 
-  const [writeTitle, setWriteTitle] = "";
+  const [writePost, setWritePost] = useState({
+    title: "",
+    content: "",
+  });
 
-  
 
-  const handleWrite = () => {
+  const handleWrite = (e) => {
+    setWritePost({
+      ...writePost,
+      [e.target.name]:e.target.value
+    });
 
+  }
+
+  const submitWrite = (e) => {
+    fetch("http://localhost:8090/api/boards", {
+      method: "POST",
+      headers: {
+        "Content-Type":"application/json; charset=utf-8"
+      },
+      body:JSON.stringify(writePost)
+    }).then(res => res.json()).then(res=>{
+      console.log(res);
+    });
   }
 
   return (
     <>
+     
       <WriteModalTitleStyle>
         <span onClick={() => setModalOpen(false)} className="modal-close">
           X
         </span>
         <div className='title-container'>
           <h2>제목</h2>
-          <input className="title-input" type="text" placeholder='제목'
-          onChange={(setWriteTitle)=> console.log(setWriteTitle) }></input>
+          <input type="text" placeholder='제목' name="title"
+          onChange={handleWrite}></input>
         
         </div>
       </WriteModalTitleStyle>
       <WriteModaContentlStyle>
         <div className='content-container'>
           <h3>내용</h3>
-          <textarea className='text-area' placeholder='내용'></textarea>
-          <button className='submit-button'>입력</button>
+          <textarea placeholder='내용' name='content' onChange={handleWrite}></textarea>
         </div>
       </WriteModaContentlStyle> 
+      <button className='submit-button' onClick={submitWrite}>입력</button>
     </>      
     
     );
